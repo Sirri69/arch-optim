27741a8ac133f3f6578fb6ca042228509f644e22
Sirri69 <pranav2278@gmail.com>
2024-08-27 21:34:41 +0530

    Added intermidiate layers and norms

--- README.md
+++ README.md
@@ -13,3 +13,42 @@ Another thing to try is to use a custom differentiable scheduler for the learnin
 
 Also, during inference, train a tree based model to predict the next tokens and assist the model during high logprob regions. The classifier will be able to switch to the main model by haveing a class that switches to __MODEL__ class.
 
+
+
+
+------------------------------------------------------------------
+## Scratch Pad
+
+GPT arch:
+
+GPT(
+  (transformer): ModuleDict(
+    (wte): Embedding(50304, 768)
+    (wpe): Embedding(1024, 768)
+    (drop): Dropout(p=0.0, inplace=False)
+    (h): ModuleList(
+      (0-11): 12 x Block(
+        (ln_1): LayerNorm()
+        (attn): CausalSelfAttention(
+          (c_attn): Linear(in_features=768, out_features=2304, bias=False)
+          (c_proj): Linear(in_features=768, out_features=768, bias=False)
+          (attn_dropout): Dropout(p=0.0, inplace=False)
+          (resid_dropout): Dropout(p=0.0, inplace=False)
+        )
+        (ln_2): LayerNorm()
+        (mlp): MLP(
+          (c_fc): Linear(in_features=768, out_features=3072, bias=False)
+          (gelu): GELU(approximate='none')
+          (c_proj): Linear(in_features=3072, out_features=768, bias=False)
+          (dropout): Dropout(p=0.0, inplace=False)
+        )
+      )
+    )
+    (ln_f): LayerNorm()
+  )
+  (lm_head): Linear(in_features=768, out_features=50304, bias=False)
+)
+
+So, to add multiple output nodes, let's add them at the blocks 8, 10, 12, the loss ratios will be 0.05, 0.15, 0.8 respectively. These intermidiate nodes will get their own lm_heads.
+
+The last output is the final output, others are intermediate outputs which will be used for helping the model during training.
--- model.py
+++ model.py
@@ -130,7 +130,15 @@ class GPT(nn.Module):
             h = nn.ModuleList([Block(config) for _ in range(config.n_layer)]),
             ln_f = LayerNorm(config.n_embd, bias=config.bias),
         ))
-        self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
+        self.lm_head_main = nn.Linear(config.n_embd, config.vocab_size, bias=False)
+        
+        # adding more LM heads for intermediate outputs
+        self.ln_inter_1 = LayerNorm(config.n_embd, bias=config.bias)
+        self.lm_head_inter_1 = nn.Linear(config.n_embd, config.vocab_size, bias=False)
+        self.ln_inter_2 = LayerNorm(config.n_embd, bias=config.bias)
+        self.lm_head_inter_2 = nn.Linear(config.n_embd, config.vocab_size, bias=False)
+
+
         # with weight tying when using torch.compile() some warnings get generated:
         # "UserWarning: functional_call was passed multiple values for tied weights.
         # This behavior is deprecated and will be an error in future versions"
@@ -177,14 +185,30 @@ class GPT(nn.Module):
         tok_emb = self.transformer.wte(idx) # token embeddings of shape (b, t, n_embd)
         pos_emb = self.transformer.wpe(pos) # position embeddings of shape (t, n_embd)
         x = self.transformer.drop(tok_emb + pos_emb)
-        for block in self.transformer.h:
+        for i, block in enumerate(self.transformer.h):
             x = block(x)
+            if i == 8:
+                inter_out_ln_1 = self.ln_inter_1(x)
+                inter_out_1 = self.lm_head_inter_1(inter_out_ln_1)
+                
+            if i == 10:
+                inter_out_ln_2 = self.ln_inter_2(x)
+                inter_out_2 = self.lm_head_inter_2(inter_out_ln_2)
+        
         x = self.transformer.ln_f(x)
 
         if targets is not None:
             # if we are given some desired targets also calculate the loss
-            logits = self.lm_head(x)
+            logits = self.lm_head_main(x)
             loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1), ignore_index=-1)
+            
+            logits_inter_1 = self.lm_head_inter_1(inter_out_ln_1)
+            loss_inter_1 = F.cross_entropy(logits_inter_1.view(-1, logits_inter_1.size(-1)), targets.view(-1), ignore_index=-1)
+            
+            logits_inter_2 = self.lm_head_inter_2(inter_out_ln_2)
+            loss_inter_2 = F.cross_entropy(logits_inter_2.view(-1, logits_inter_2.size(-1)), targets.view(-1), ignore_index=-1)
+            
+            loss = 0.8*loss + 0.05*loss_inter_1 + 0.15*loss_inter_2
         else:
             # inference-time mini-optimization: only forward the lm_head on the very last position
             logits = self.lm_head(x[:, [-1], :]) # note: using list [-1] to preserve the time dim
